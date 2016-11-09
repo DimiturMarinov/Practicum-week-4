0cfd392d4543c6f3f893e91a14fe1d4a209df7ae
DimiturMarinov <dim.marinov@abv.bg>
2016-11-10 01:44:40 +0200

    Add files via upload

---
+++ zad 6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	int A;
+	for (int i = 1; i <= n; i++){
+		A = i*i + 3 * i;
+		cout << A <<endl;
+}
+	
+
+
+	system("pause");
+	return 0;
+}
